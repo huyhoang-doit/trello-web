f2318c1673d57445e813d473abe268021dda4059
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-08-18 09:53:56 +0700

    mongodb aggregate be

--- src/pages/Boards/Boards.jsx
+++ src/pages/Boards/Boards.jsx
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI } from '~/apis'
 function Board() {
@@ -10,7 +10,7 @@ function Board() {
 
   useEffect(() => {
     // react-router-dom
-    const boardId = '66c04fe1406b02ab98797e09'
+    const boardId = '66c14c41622f69315ca4e6f1'
     // Call api
     fetchBoardDetailsAPI(boardId)
       .then((data) => setBoard(data))
