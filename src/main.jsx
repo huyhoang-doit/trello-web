4747a8893de51e60c0aa309ff54205cdd3dc1101
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-08-24 10:50:28 +0700

    delete column and cards in column

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from '~/App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from '~/theme'
+// MUI Dialog
+import { ConfirmProvider } from 'material-ui-confirm'
 
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -11,9 +13,16 @@ import 'react-toastify/dist/ReactToastify.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-      <ToastContainer theme="colored" />
+      <ConfirmProvider defaultOptions={{
+        allowClose: false,
+        dialogProps: { maxWidth: 'xs' },
+        confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+        cancellationButtonProps: { color: 'inherit' }
+      }}>
+        <CssBaseline />
+        <App />
+        <ToastContainer theme="colored" />
+      </ConfirmProvider>
     </CssVarsProvider>
   </React.StrictMode>
 )
