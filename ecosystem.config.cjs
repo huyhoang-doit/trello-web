807cf543640edd62982f43ebbace617edbfd69a6
Le Van Huy Hoang <huyhoangdoit.171@gmail.com>
2025-02-16 16:47:47 +0000

    a

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,11 +2,7 @@ module.exports = {
   apps: [
     {
       name: "trello-clone-web",
-      script: "cmd",
-      args: "/c npm run preview", // Chạy npm trong cmd
-      interpreter: "none", // Không cần interpreter
-      autorestart: true,
-      watch: false,
+      script: "npm run preview -- --host",
     },
   ],
-}; 
+};
